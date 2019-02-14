e14c2c51bd29e2dec07df101ff2df65c59c39f2e
Zhanaikhanov <bekablack.bz@gmail.com>
2019-02-14 09:32:28 +0600

    Update wsgi.py

--- zerkala12/wsgi.py
+++ zerkala12/wsgi.py
@@ -8,9 +8,24 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import time 
+import traceback 
+import signal 
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/zerkala12.kz') 
+# adjust the Python version in the line below as needed 
+sys.path.append('/home/envs/zerkala12.kz/lib/python3.6/site-packages') 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zerkala12.settings')
 
-application = get_wsgi_application()
+try: 
+    application = get_wsgi_application() 
+except Exception: 
+    # Error loading applications 
+    if 'mod_wsgi' in sys.modules: 
+        traceback.print_exc() 
+        os.kill(os.getpid(), signal.SIGINT) 
+        time.sleep(2.5) 
